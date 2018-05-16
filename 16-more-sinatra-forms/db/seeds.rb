36923fd1e7f35a3edd01e83f8d6ac21f785c785d
nikymorg <nicole.morgan@gmail.com>
2018-05-15 20:56:39 -0400

    add author seeds

--- 16-more-sinatra-forms/db/seeds.rb
+++ 16-more-sinatra-forms/db/seeds.rb
@@ -1,3 +1,6 @@
-Book.destroy_all
-
 GoogleBooks::Adapter.new("Roald Dahl").fetch_books
+
+# Author.create(name: "Roald Dahl")
+# Author.create(name: "Neil Gaiman")
+# Author.create(name: "Beatrix Potter")
+# Author.create(name: "Jim Campagno")
