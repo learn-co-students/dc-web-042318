6c38b79146e22073cbe4853282caf8ea279dcec3
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-05-21 14:43:52 -0400

    prelecture

---
+++ 19-rails-associations/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+snacks = Snack.create([{name: "Thin Mints", calories: 200, deliciousness: 6},
+                       {name: "Chicken fingers", calories: 650, deliciousness: 8}])
