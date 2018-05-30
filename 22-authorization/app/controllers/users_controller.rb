669b5e47345be0f3e998456e3cdd9620b4162f95
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-05-30 10:45:40 -0400

    added user logic

--- 22-authorization/app/controllers/users_controller.rb
+++ 22-authorization/app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new()
+    @user.attributes = user_params
+    if @user.valid?
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to snacks_path
+    else
+        flash[:error] = @user.errors.full_messages
+        redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
 end
