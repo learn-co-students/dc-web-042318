f15a821157ac5c6973ecc0c7b314418fba86b03e
Rob Cobb <rwcobbjr@gmail.com>
2018-06-08 11:17:21 -0400

    fetch and post

--- 31-fetch-and-post/README.md
+++ 31-fetch-and-post/README.md
@@ -1,11 +1,19 @@
 # Fetch and Post
 
 Goal: Single Page Application
+  - load things async w/o a page reload
+  - change the address bar and the url without doing a page reload
 
 ## Review
+
 fetch
 
 ## Post
+
+specify with second arg to fetch
+options object
+method: POST
+body: has to be a string
 
 ## Characters Challenge
 
@@ -13,10 +21,16 @@ Master / Detail view
 
 I should be able to:
 
-- see a list of all characters
-- view the detail of a character
+X see a list of all characters
+
 - listen for clicks on the character
 - fetch the character when clicked
 - render the detail view of the character when the fetch returns
+- view the detail of a character
 - when we click a different character, replace  the detail view
-- update the character's name and location
+
+HW:
+- styling
+- prevent duplicates
+- select the monster when submitting (and add to list)
+- update the character's name and location and img
--- 31-fetch-and-post/index.js
+++ 31-fetch-and-post/index.js
@@ -1,20 +1,116 @@
-console.log("Hello, monsters");
+const url = "http://localhost:3000/characters";
 
-// function listString(character) {
-// return `<li data-id="${character.id}">${character.name}</li>`;
-// }
+function getCharacters() {
+  // returns  a Promise for the actual characters data
+  return fetch(url).then(res => res.json());
+}
 
-// function detailView(character) {
-//   return `<div>
-//   <img src="${character.img}" />
-//   <h2>${character.name}</h2>
-//   <h3>of ${character.location}</h3>
-//   </div>`;
-// }
+function listString(character) {
+  return `<li data-id="${character.id}">${character.name}</li>`;
+}
 
-// function getFormData() {
-//   let name = document.querySelector("#name").value;
-//   let location = document.querySelector("#location").value;
-//   let img = document.querySelector("#img").value;
-//   return { name, location, img };
-// }
+// Strategy: iterate through the returned characters
+// call the listString on each character
+// want to append this to the dom
+// find the ul
+
+function getCharacterListUl() {
+  return document.querySelector("ul.character-list");
+}
+
+function loadListOfCharacters() {
+  return getCharacters().then(characters =>
+    characters.forEach(character => {
+      let li = listString(character);
+      getCharacterListUl().innerHTML += li;
+    })
+  );
+}
+
+function findListItems() {
+  return document.querySelectorAll("li");
+}
+
+function getDetailViewDiv() {
+  return document.querySelector(".detail-view");
+}
+
+// make the fetch request
+// deal with the promise
+// render the detail view x
+function loadCharacterDetailView(characterId) {
+  // console.log(characterId);
+  // console.log(url + "/" + characterId);
+  fetch(url + "/" + characterId)
+    .then(res => res.json())
+    .then(character => {
+      let div = detailView(character);
+      getDetailViewDiv().innerHTML = div;
+    });
+}
+
+function attachEventListeners() {
+  findListItems().forEach(item => {
+    item.addEventListener("click", e => {
+      // console.log(item.dataset.id);
+      // console.log(e.target.dataset.id);
+      loadCharacterDetailView(item.dataset.id);
+    });
+  });
+}
+
+// Viewing the detail view
+
+// find the elements
+// attach Event listener(s)
+// which one did we click?
+
+function detailView(character) {
+  return `<div>
+  <img src="${character.img}" />
+  <h2>${character.name}</h2>
+  <h3>of ${character.location}</h3>
+  </div>`;
+}
+
+// Create a character
+
+function getForm() {
+  return document.querySelector("form");
+}
+
+// Create a new character
+// X listen for the form submit
+// X prevent default
+// X get the form data
+// X send a post request
+// with the form data
+
+function submitData(data) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json", Accept: "application/json" }
+  });
+}
+
+function listenForFormSubmit() {
+  getForm().addEventListener("submit", e => {
+    e.preventDefault();
+    submitData(getFormData());
+  });
+}
+
+function getFormData() {
+  let name = document.querySelector("#name").value;
+  let location = document.querySelector("#location").value;
+  let img = document.querySelector("#img").value;
+  return { name, location, img };
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadListOfCharacters().then(() => {
+    attachEventListeners();
+  });
+  listenForFormSubmit();
+});
