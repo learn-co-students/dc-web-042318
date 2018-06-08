4ca758b97350f0c6beaacddb63dea2ab899b70ae
Rob Cobb <rwcobbjr@gmail.com>
2018-06-08 09:43:04 -0400

    pre lecture

---
+++ 31-fetch-and-post/README.md
@@ -0,0 +1,22 @@
+# Fetch and Post
+
+Goal: Single Page Application
+
+## Review
+fetch
+
+## Post
+
+## Characters Challenge
+
+Master / Detail view
+
+I should be able to:
+
+- see a list of all characters
+- view the detail of a character
+- listen for clicks on the character
+- fetch the character when clicked
+- render the detail view of the character when the fetch returns
+- when we click a different character, replace  the detail view
+- update the character's name and location
---
+++ 31-fetch-and-post/index.js
@@ -0,0 +1,20 @@
+console.log("Hello, monsters");
+
+// function listString(character) {
+// return `<li data-id="${character.id}">${character.name}</li>`;
+// }
+
+// function detailView(character) {
+//   return `<div>
+//   <img src="${character.img}" />
+//   <h2>${character.name}</h2>
+//   <h3>of ${character.location}</h3>
+//   </div>`;
+// }
+
+// function getFormData() {
+//   let name = document.querySelector("#name").value;
+//   let location = document.querySelector("#location").value;
+//   let img = document.querySelector("#img").value;
+//   return { name, location, img };
+// }
