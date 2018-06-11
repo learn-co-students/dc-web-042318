7f3d71e6e353cfbaeebcc22df8348433839715e3
Rob Cobb <rwcobbjr@gmail.com>
2018-06-11 16:27:52 -0400

    object oriented js

--- 33-oo-js/README.md
+++ 33-oo-js/README.md
@@ -6,17 +6,68 @@
 - `class` syntax
 
 Why object oriented programming?
+- Reuse code
+- Organize code
+- 'responsibilities'
+- group data and behavior
+- map real world problems to our code
+
+```
+person.greet()
+// 'hi'
+greet(person);
+// 'hi'
+```
+
 What should we be able to do with objects?
+- x attributes (store some data on our objects) (instance variables)
+   - literal
+- x (dry up our object creation) (create many instances)
+   - factory pattern
+- x methods (call some function associated with the object)
+- x instance methods - behavior of one of these kinds of objects
+  - privacy (no way to indicate privacy in js, ignore)
+
+- class methods - behavior associated with the whole class
+- class variables - data kept between calls
+
+- inherit characteristics between classes
+  - Car is a Vehicle
+  - Plane is a Vehicle
+
+- x instances of objects have relationships with each other
+  - x Teacher has many students
 
 ## Object Creation Patterns
 
 - literal
-- 'Factory'
+- Factory pattern
 - Object.create
 - `new` Object
 
-## `Object.create`, Prototype Chain, and `__proto__`
+## `Object.create`, Prototype Chain, and `__proto__` delegation
+
+when we try to access a property
+  -> if it exists in the object's keys, return it
+  -> if it does not -> look up the prototype chain (look in the __proto__ object)
+
+not everything has a __proto__ - Object.create(null), we don't have access to any default object (properties) methods
+when we create an object with the literal syntax, it gets the 'root' object as __proto__
+
+Object.create -- sets a new object's __proto__ to the argument
 
 ## `new` and Constructor calls
 
-## Class syntax
+special call `new` - different behavior
+  - this = new object with __proto__ set to the function's prototype
+  - (don't follow the other this rules)
+  - automatically returned the object
+
+.prototype
+
+## `class` syntax
+
+sugar around functions that have constructor calls
+
+methods on the prototype of the object
+when we invoke with `new`, the function's prototype gets set to the new object's __proto__
--- 33-oo-js/index.js
+++ 33-oo-js/index.js
@@ -1,1 +1,113 @@
 "use strict";
+
+// console.log("welcome to objects");
+//
+// // literal
+//
+// // let champs = {
+// //   name: "Capitals"
+// // };
+//
+// // let nextChamps = {
+// // name: "Capitals"
+// // };
+//
+// // Factory pattern
+//
+// // create instances
+// // parameterize our instance creation
+//
+// function celebrate() {
+//   console.log(
+//     "WOOOH! the ",
+//     this.name,
+//     "won the ",
+//     this.year,
+//     "Cup!!!!!!!!!!!!!"
+//   );
+// }
+//
+// let protoChamp = {
+//   celebrate
+// };
+//
+// function teamChampFactory(name, year, sport) {
+//   let newChamp = Object.create(protoChamp);
+//   newChamp.name = name;
+//   newChamp.year = year;
+//   newChamp.sport = sport;
+//   return newChamp;
+// }
+//
+// function hockeyChampFactory(year) {
+//   return teamChampFactory("Capitals", year, "Hockey");
+// }
+//
+// // prototype chain
+//
+// // Object.create
+//
+// // when we create this function, we !!!! also get a new object, .prototype, { constructor: function }
+// // `class`
+// // initialize method
+//
+// function TeamChamp(year) {
+//   this.year = year;
+// }
+//
+// function HockeyChamp(year) {
+//   this.name = "Capitals";
+//   this.sport = "hockey";
+//   this.fans = [];
+//   HockeyChamp.all.push(this);
+// }
+// HockeyChamp.all = [];
+//
+// HockeyChamp.prototype = Object.create(TeamChamp.prototype);
+// HockeyChamp.prototype.addFan = function(fan) {
+//   this.fans.push(fan);
+// };
+//
+// let champ = new HockeyChamp(2018); // champ.__proto__.__proto__ === TeamChamp.prototype
+//
+// // instance method!
+// HockeyChamp.prototype.celebrate = celebrate;
+//
+// // constructor call
+// // let champs = new HockeyChamp(2018);
+// // champs.__proto__ === HockeyChamp.prototype
+//
+//
+//
+//
+
+class HockeyChamp {
+  constructor(year) {
+    this.year = year;
+  }
+
+  celebrate() {
+    console.log("Yay!");
+  }
+}
+
+class User {
+  constructor() {
+    User.all.push(this);
+  }
+
+  login() {}
+
+  save() {}
+
+  render() {}
+
+  static saveAll() {}
+}
+
+User.all = [];
+let Rob = new User();
+// renderUser({});
+// saveUser({});
+User.saveAll = function() {};
+User.saveAll();
