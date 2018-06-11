bfd12566cfc389c3d8a4afe398878e77da27440a
Rob Cobb <rwcobbjr@gmail.com>
2018-06-11 11:10:29 -0400

    this, bind, call, apply

--- 32-oo-js/README.md
+++ 32-oo-js/README.md
@@ -2,12 +2,65 @@
 
 ## `this` and Function Execution Context
 
+Rules:
+1. `this` in the global context is window (or 'global' in node)
+  a. in a function, in strict mode, undefined (sloppy mode - global)
+2. when a function is called as a 'method', `this` is the object
+3. can control `this` using bind, call, and apply
+    this is the first argument to the method
+4. arrow functions get the `this` from the enclosing scope
+
 ### Strict Mode
+
+"use strict";
+
+Raise more errors
+
+`this` no longer the global object
+
 
 ### Object 'Methods'
 
 js `this` vs. ruby `self`
 
+self is defined by where we wrote the method
+
+```
+class Dog
+  def bark
+    puts "#{self.noise}"
+  end
+end
+```
+
+in js, `this` is defined by where we _call_ the method
+
+
+How do we control what `this` will be?
+
 ## `bind`, `call`, `apply`
 
+`bind` - lets us set the `this` object to an argument
+- returns a new function, doesn't modify the function
+- cannot rebind to some new context
+- can pass in additional args to 'set' them, get back a function with those args preset
+   - used for 'partial application'
+
+HW: can we tell if a function has been bound?
+
+`call` - calls the function
+ - bind still overrides
+ - first argument gets assigned to `this`
+ - subsequent args get passed through
+
+`apply` - calls the function
+- same as call
+- except!
+- takes an array as the second argument
+
 ## Arrow Functions
+
+now `this` is defined by where the function is written
+(gets the same `this` as its enclosing scope)
+
+(similar to the `var self = this` pattern)
--- 32-oo-js/index.js
+++ 32-oo-js/index.js
@@ -1,12 +1,85 @@
+"use strict";
+
 // Objects can have function properties
 let person = {
-  name: "Rob",
+  name: "Person",
   occupation: "Instructor"
 };
 
 function sayGreeting(name, greeting) {
+  console.log("this is", this);
   return name + " says " + greeting;
 }
 
-person.greet = sayGreeting;
-person.greet(person.name, "hello"); // => "Rob says hello"
+// sayGreeting("Sam", "TGIM");
+
+let greeting = sayGreeting.bind(person);
+// let result = person.greet(person.name, "hello"); // => "Rob says hello"
+// console.log(result);
+
+let rob = {
+  name: "Rob",
+  occupation: "Instructor",
+  students: [1, 2, 3, 4],
+  greet: () => {
+    // var self = this;
+    // func(item, index, arr);
+    console.log(this);
+    rob.students.forEach(student => {
+      // what is this?
+      // rob
+      // global
+      // undefined
+      console.log(this);
+    });
+  }
+};
+
+// what is this?
+// rob object
+// undefined
+// global
+
+rob.greet();
+// let greet = rob.greet;
+// greet();
+
+// set the `this` object
+// let boundGreet = rob.greet.bind(rob);
+
+// let sam = {
+//   name: "Sam",
+//   greet: rob.greet.bind(rob, "Bird")
+// };
+//
+// let greet = rob.greet;
+//
+// // greet.call(sam, "Bird", "Is the Word");
+// greet.apply(sam, ["Bird", "Is the Word"]);
+
+// sam.greet("The Word");
+
+// what will `this` be?
+// rob object
+// sam object
+// window
+// undefined
+
+// rob.greet();
+// sam.greet();
+// greet();
+
+// console.log(rob.greet === sam.greet);
+
+// function multiplyBy(multiplier) {
+//   return function mult(x) {
+//     return x * multiplier;
+//   };
+// }
+//
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// let multByTwo = multiplyBy(2);
+// let multByTwo = multiply.bind(null, 2);
